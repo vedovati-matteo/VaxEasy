d1c77156945aef836abd3bbef89fb0fec124fd91
AlessandroBelotti106721 <67004113+AlessandroBelotti106721@users.noreply.github.com>
2021-06-07 19:00:55 +0200

    modifiche

--- model_utils/prenotazioni.py
+++ model_utils/prenotazioni.py
@@ -14,11 +14,7 @@ class Prenotazione(db.Model):
         self.codice_appuntamento = codice_appuntamento
 
     def __repr__(self):
-        return "Appuntamento-{}: {} - {}".format(self.cf, self.get_full_name(), self.password)
-
-    # Method to get the user full name
-    def get_full_name(self):
-        return "{nome} {cognome}".format(cognome=self.cognome, nome=self.nome)
+        return "Prenotazione-{}: {} - {}".format(self.cf, self.vaccino, self.codice_appuntamento)
 
     # Method to convert a user in JSON format to store it in session["user"]
     def to_json(self):
@@ -27,26 +23,5 @@ class Prenotazione(db.Model):
     def __setstate__(self, state):
         self.__dict__.update(state)
 
-
-# Create a User object from its json representation in session["user"]
-def get_user_from_json(json):
-    return jsonpickle.decode(json)
-
-
-# Recover all users in the database
-def get_users():
-    return {user.cf:user for user in Utente.query.all()}
-
-
-# Recover a user by its CF
-def get_user_by_cf(cf):
-    return Utente.query.filter_by(cf=cf).first()
-
-
-
-# Add a new user to the database
-def add_user(new_cf, new_nome, new_cognome, new_email, new_cell, new_prov):
-    db.session.add(Utente(new_cf, new_nome, new_cognome, new_email, new_cell, new_prov))
-    db.session.commit()
 
 
---
+++ model_utils/vaccini.py
@@ -0,0 +1,44 @@
+import jsonpickle
+from app import db
+
+
+# Class representing users
+class Vaccino(db.Model):
+    codice = db.Column(db.Text(), unique=True, primary_key=True)
+    nome = db.Column(db.Text(), nullable=False)
+    descrizione = db.Column(db.Text(), nullable=False)
+
+    def __init__(self, codice, nome, descrizione):
+        self.codice = codice
+        self.nome = nome
+        self.descrizione = descrizione
+
+    def __repr__(self):
+        return "Vaccino-{}: {} - {}".format(self.codice, self.nome, self.descrizione)
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+
+
+# Create a User object from its json representation in session["user"]
+def get_vax_from_json(json):
+    return jsonpickle.decode(json)
+
+
+# Recover all vax in the database
+def get_vax():
+    return {vaccini.codice:vaccini for vaccini in Vaccino.query.all()}
+
+
+# Recover a vax by its code
+def get_vax_by_code(cf):
+    return Vaccino.query.filter_by(codice=codice).first()
+
+
+
+# Add a new vax to the database
+def add_user(new_code, new_nome, new_description):
+    db.session.add(Vaccino(new_code, new_nome, new_description))
+    db.session.commit()
+
+
