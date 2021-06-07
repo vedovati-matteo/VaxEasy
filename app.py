5a6fa0942f1933bcd7583d040a9d6a1738e2ddd3
vedovati-matteo <68272450+vedovati-matteo@users.noreply.github.com>
2021-06-07 17:16:19 +0200

    aggiunta1

--- app.py
+++ app.py
@@ -2,14 +2,6 @@ from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
 
-#@app.before_request
-#def check_login():
-    # Check if the requested page can be accessed without being logged
-    
-    # If the user is not logged redirect to the login page
-    #if not (session.get("user")) and request.endpoint != "login":
-    #   return redirect(url_for("login"))
-
 @app.route('/')
 def home():
 	return render_template("basic/home.html")
