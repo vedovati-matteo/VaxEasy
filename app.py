b4bd222f48bdee9160d96363ab767824bdd61181
vedovati-matteo <68272450+vedovati-matteo@users.noreply.github.com>
2021-06-14 12:27:53 +0200

    coso

--- app.py
+++ app.py
@@ -112,17 +112,10 @@ def landLogin():
 
 	if len(campi[0]) > 0:
 		# check password
-		if check_password(campi[0][0].cf, request.form.get("pw")):
-			par = {
-				"cf": campi[0][0].cf,
-				"nome": campi[0][0].nome,
-				"cognome": campi[0][0].cognome,
-				"provincia": campi[0][0].provincia,
-				"tel": campi[0][0].telefono,
-				"email": campi[0][0].mail,
-				"patologie": campi[1]
-			}
-			session["user"] = par
+		par = Utente.__repr__(campi[0])
+		if check_password(campi[0][0], request.form.get("pw")):
+	
+			session["user"] = campi
 			return redirect(url_for("home"))
 	
 	return redirect(url_for("login"))
@@ -150,23 +143,11 @@ def landSignin():
 		"patologie": patologie
 	}
 
-	add_user(
-		request.form.get("cf"),
-		request.form.get("nome"),
-		request.form.get("cognome"),
-		request.form.get("email"),
-		request.form.get("tel"),
-		request.form.get("provincia"),
-		request.form.get("pw"),
-		patologie
-	)
-
-	"""
 	if createUser(campi):
 		return redirect(url_for("login"))
 	else:
 		return redirect(url_for("signin"))
-	"""
+
 
 # ====> PROFILO
 @app.route('/profilo')
@@ -184,8 +165,7 @@ def logout():
 # ====> PRENOTAZIONE
 @app.route('/listaAppuntamenti')
 def listaAppuntamenti():
-	print("=============Z")
-	print(get_appuntamentoByProvincia("Bergamo"))
+
 	return render_template("prenotazione/listaAppuntamenti.html", appuntamenti=getAppuntamenti(session["user"]["provincia"]))
 
 @app.route('/prenotazione', methods=["GET"])
@@ -215,8 +195,7 @@ def listaPrenotazioni():
 
 if __name__ == '__main__':
 	from model_utils.patologie import get_patologia
-	from model_utils.user import get_user_by_cf, check_password, add_user
-	from model_utils.appuntamenti import get_appuntamentoByProvincia
+	from model_utils.user import get_user_by_cf, check_password, Utente
 	app.run(host='127.0.0.1', port=5000, debug=True)
 	
 
